7ccdda7a3a348601e9306c30d57a7123ff5e87ae
Rebecca Pattichis <pattichi@stanford.edu>
2021-07-22 12:51:17 -0600

    testing pt 2

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Hello</Text>
+      <Text>Hello how's it going</Text>
       <StatusBar style="auto" />
     </View>
   );
