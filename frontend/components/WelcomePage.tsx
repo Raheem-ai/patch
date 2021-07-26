e2fa1e6b03549b51f8b59389c215aff82789651c
Rebecca Pattichis <pattichi@stanford.edu>
2021-07-26 12:20:35 -0600

    fixed routing bug

--- frontend/components/WelcomePage.tsx
+++ frontend/components/WelcomePage.tsx
@@ -1,18 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
 import * as React from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function WelcomePage() {
+export default function WelcomePage({ navigation }) {
     return (
         <View style={styles.container}>
             <Text>Welcome to Patch by Raheem!</Text>
-            <Button mode="contained">Sign In</Button> 
-            <Button mode="contained">Sign Up</Button>
-        </View>
+            <Button mode="contained" onPress={() => navigation.navigate('SignIn')}>Sign In</Button> 
+            <Button mode="contained" onPress={() => navigation.navigate('SignUp')}>Sign Up</Button>
+        </View> 
     );
 };
-
 
 const styles = StyleSheet.create({
     container: {
