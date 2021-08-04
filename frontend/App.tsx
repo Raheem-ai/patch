67994afc313c3b8396c0f3195b85c454bb8ed5d3
Rebecca Pattichis <pattichi@stanford.edu>
2021-08-03 22:51:55 -0600

    weird bug for theme declaration, think its type issue again w colors

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, Provider as PaperProvider } from 'react-native-paper';
 import { Switch, Route, BrowserRouter as Router, useHistory } from 'react-router-dom';
 import "react-native-gesture-handler";
 
@@ -12,23 +12,40 @@ import SignUpForm from './src/components/SignUpForm';
 import UserHomePage from './src/components/userside/UserHomePage';
 
 // navigating imports
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList, routerNames } from './src/types';
+import { Provider } from 'react-native-paper/lib/typescript/core/settings';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-const theme = {};
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+  fonts: {
+    regular: 'OTF',
+  },
+  animation: {
+    
+  },
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={routerNames.home} component={WelcomePage} />
-        <Stack.Screen name={routerNames.signIn} component={SignInForm} />
-        <Stack.Screen name={routerNames.signUp} component={SignUpForm} />
-        <Stack.Screen name={routerNames.userHome} component={UserHomePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={routerNames.home} component={WelcomePage} />
+          <Stack.Screen name={routerNames.signIn} component={SignInForm} />
+          <Stack.Screen name={routerNames.signUp} component={SignUpForm} />
+          <Stack.Screen name={routerNames.userHome} component={UserHomePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
