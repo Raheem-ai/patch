1fbfd0170aded9c73bf1f5e7aa83cb954901ab38
Rebecca Pattichis <pattichi@stanford.edu>
2021-08-12 13:24:22 -0600

    jest errors for component testing lol

---
+++ frontend/src/test/signin.test.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { SignUpForm } from '../components/SignUpForm';
+
+test('basic', () => {
+    const checkbox = shallow(<SignUpForm />);
+
+  });
