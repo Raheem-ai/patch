6b4bccc68826d3a1ff48b92279bb0209eb426a15
Charlie Lipford <charlie.lipford@gmail.com>
2021-10-26 11:22:50 -0400

    testing

--- backend/functions/deployment/gc.sh
+++ backend/functions/deployment/gc.sh
@@ -10,6 +10,7 @@ IMAGE_REPO="gcr.io/$PROJECT_ID/patch"
 # Get all images at the given image repo
 echo -e "${YELL}Getting all images for repo ${IMAGE_REPO}${NC}"
 IMAGELIST=$(gcloud container images list --repository=${IMAGE_REPO} --format='get(name)')
+gcloud container images list --repository=${IMAGE_REPO} --format='get(name)'
 echo "${IMAGELIST}"
 
 while IFS= read -r IMAGENAME; do
