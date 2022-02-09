8d498204ba71361145876f268a5917e8b4be1119
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-09 18:12:29 -0500

    use staging to cache if exists

--- ci/backend/build_temp.sh
+++ ci/backend/build_temp.sh
@@ -1,14 +1,21 @@
 IMAGE_REPO=patch-tmp
 IMAGE=gcr.io/$PROJECT_ID/$IMAGE_REPO:$BRANCH_NAME
+# STAGING_IMAGE=gcr.io/$PROJECT_ID/patch-rc:latest
+STAGING_IMAGE=gcr.io/$PROJECT_ID/patch:rc-latest
 
 build_temp () {
     # try and pull tmp image from registry
     if docker pull $IMAGE ; then
-        # build using cache
+        # build using cache from this branch
         docker build -t $IMAGE -f api.dockerfile --cache-from $IMAGE .
     else
-        # build fresh
-        docker build -t $IMAGE -f api.dockerfile .
+        if docker pull $STAGING_IMAGE ; then
+            # build using cache from staging (which this branch should have forked from)
+            docker build -t $IMAGE -f api.dockerfile --cache-from $STAGING_IMAGE .
+        else 
+            # build fresh
+            docker build -t $IMAGE -f api.dockerfile --cache-from $IMAGE .
+        fi
     fi
 }
 
