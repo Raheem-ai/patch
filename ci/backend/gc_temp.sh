af2c91bd0791d8338a33a9baecd0f5639ead90b8
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-09 18:47:14 -0500

    trying labels

--- ci/backend/gc_temp.sh
+++ ci/backend/gc_temp.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 IMAGE_REPO=patch-tmp
 BASE_IMAGE=gcr.io/$PROJECT_ID/$IMAGE_REPO
-IMAGE=$BASE_IMAGE:$BRANCH_NAME
 IMAGES_TO_KEEP="1"
 
 # only keep one copy of each tmp image
 clean_dup_images(){
   local C=0
-  for digest in $(gcloud container images list-tags ${IMAGE} --limit=999999 --sort-by=~TIMESTAMP --format='get(digest)'); do
+  for digest in $(gcloud container images list ${BASE_IMAGE} --filter label=branch=$BRANCH_NAME --limit=999999 --sort-by=~TIMESTAMP --format='get(digest)'); do
     (
         if [ $C -lt $IMAGES_TO_KEEP ]
         then
             echo keeping ${digest}
         else
             echo deleting ${digest}
-            gcloud container images delete -q --force-delete-tags "${IMAGE}@${digest}"
+            # gcloud container images delete -q --force-delete-tags "${BASE_IMAGE}@${digest}"
         fi
     )
     let C=C+1
