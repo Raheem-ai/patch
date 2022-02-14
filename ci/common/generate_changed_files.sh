6c792aaa32df9a09df4aa7bd238939d8edb844c1
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-14 16:46:25 -0500

    bringing in new deploy key

--- ci/common/generate_changed_files.sh
+++ ci/common/generate_changed_files.sh
@@ -1,3 +1,8 @@
+echo \"$$PATCH_SSH_KEY\" >> /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-keyscan -t rsa github.com > known_hosts.github
+cp known_hosts.github /root/.ssh/known_hosts
+
 echo git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
 git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
 
