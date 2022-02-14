a0e9827e408775eb7ad1e7020af5425e41b5ca57
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-14 15:52:24 -0500

    trying specific repo

--- ci/common/generate_changed_files.sh
+++ ci/common/generate_changed_files.sh
@@ -1,5 +1,5 @@
-echo git fetch
-git fetch origin
+echo git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
+git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
 
 echo git diff-tree --no-commit-id --name-only -r $_MERGED_SHA
 FILES=$(git diff-tree --no-commit-id --name-only -r $_MERGED_SHA)
