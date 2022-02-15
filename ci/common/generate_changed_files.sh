4445c845b5f9152162844be4fff79a220801bbb7
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-14 19:43:16 -0500

    using ssh config for deploy rc

--- ci/common/generate_changed_files.sh
+++ ci/common/generate_changed_files.sh
@@ -1,10 +1,10 @@
-echo \"$PATCH_SSH_KEY\" >> /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-keyscan -t rsa github.com > known_hosts.github
-cp known_hosts.github /root/.ssh/known_hosts
+# echo \"$PATCH_SSH_KEY\" >> /root/.ssh/id_rsa
+# chmod 600 /root/.ssh/id_rsa
+# ssh-keyscan -t rsa github.com > known_hosts.github
+# cp known_hosts.github /root/.ssh/known_hosts
 
-echo git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
-git fetch --depth=1 git@github.com:Raheem-ai/patch.git $_MERGED_SHA
+echo git fetch --depth=1 patch:Raheem-ai/patch.git $_MERGED_SHA
+git fetch --depth=1 patch:Raheem-ai/patch.git $_MERGED_SHA
 
 echo git diff-tree --no-commit-id --name-only -r $_MERGED_SHA
 FILES=$(git diff-tree --no-commit-id --name-only -r $_MERGED_SHA)
@@ -14,3 +14,4 @@ echo git log
 echo $(git log)
 
 git log
+
