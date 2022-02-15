312bc235672e048b3b235f6f54c8bea1137b3511
Charlie Lipford <charlie.lipford@gmail.com>
2022-02-14 20:03:21 -0500

    adding targeted fetch back

--- ci/common/generate_changed_files.sh
+++ ci/common/generate_changed_files.sh
@@ -3,15 +3,13 @@
 # ssh-keyscan -t rsa github.com > known_hosts.github
 # cp known_hosts.github /root/.ssh/known_hosts
 
-echo git fetch patch:Raheem-ai/patch.git 
-git fetch patch:Raheem-ai/patch.git 
+echo git fetch --depth=3 patch:Raheem-ai/patch.git $_MERGED_SHA
+git fetch --depth=3 patch:Raheem-ai/patch.git $_MERGED_SHA
 
 echo git diff-tree --no-commit-id --name-only -r $_MERGED_SHA
 FILES=$(git diff-tree --no-commit-id --name-only -r $_MERGED_SHA)
 echo $FILES
 
 echo git log
-echo $(git log)
-
 git log
 
