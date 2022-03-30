f99af6c344a5369b85ed474112d68d255f176caa
Charlie Lipford <charlie.lipford@gmail.com>
2022-03-30 14:54:14 -0400

    throwing my hands up

--- frontend/src/components/calendarPicker.tsx
+++ frontend/src/components/calendarPicker.tsx
@@ -1,35 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Calendar from 'react-native-calendar-picker';
 import { View, StyleSheet, Pressable } from "react-native";
 import moment from 'moment';
 
 type CalendarPickerProps = {
     onDateChange: (date: Date) => void,
-    intitalDate: Date
+    initialDate: Date
 }
 
 // TODO: 
 // - handle UI when today is the chosen day
 const CalendarPicker = ({
-    intitalDate,
+    initialDate,
     onDateChange
 }: CalendarPickerProps) => {
+
+    const [currentDate, setCurrentDate] = useState(initialDate)
+
+    const dateChanged = (mDate) => {
+        onDateChange(mDate.toDate())
+        setCurrentDate(mDate.toDate())
+    }
+
+    const today = moment();
+    const todayIsSelected = !!(currentDate && moment(currentDate).isSame(today, 'day'))
 
     // setup specific style for today's date
     const customDatesStyles = [];
     customDatesStyles.push({
-        date: moment(), //today
+        date: today, //today
         // textStyle: styles.todayTextStyle,
-        style: styles.todayStyle,
+        style: [ styles.todayStyle, todayIsSelected ? styles.selectedStyle : null ],
         // containerStyle: styles.todayContainerStyle,
         allowDisabled: true, // allow custom style to apply to disabled dates
     })
+
+    if (!todayIsSelected) {
+        // setup specific styles for the selected day when it isn't today
+        // TODO: this style wont take for some reason
+        customDatesStyles.push({
+            date: moment(currentDate).clone(), //selected date
+            textStyle: { color: styles.selectedStyle.color },
+            style: styles.selectedStyle,
+            containerStyle: [],
+            allowDisabled: true, // allow custom style to apply to disabled dates
+        })
+    }
 
     return (
         <Calendar
             weekdays={['S', 'M', 'T', 'W', 'T', 'F',  'S']}
             // translate from moment to Date
-            onDateChange={(mDate) => onDateChange(mDate.toDate())} 
+            onDateChange={dateChanged} 
 
             // need to restrict navigation with min/max to make
             // scroll behavior work right
@@ -37,21 +59,21 @@ const CalendarPicker = ({
             minDate={moment().date(1)}// first of this month
             maxDate={moment().add(1, 'y')} // a year from now
 
-            selectedStartDate={intitalDate}
-            initialDate={moment(intitalDate)}
+            selectedStartDate={initialDate}
+            initialDate={moment(initialDate)}
             showDayStragglers={true}
             scrollable={true}
 
             headerWrapperStyle={{ height: 0, margin: 0, padding: 0 }}
             dayLabelsWrapper={{ borderBottomWidth: 0, borderTopWidth: 0 }}
             
-            // textStyle={{ color: styles.selectedStartDate.backgroundColor }}
-            selectedDayColor={styles.selectedStartDate.backgroundColor}
-            selectedDayTextColor={styles.selectedStartDate.color}
+            textStyle={{ color: styles.selectedStartDate.backgroundColor }}
+            // selectedDayColor={styles.selectedStartDate.backgroundColor}
+            // selectedDayTextColor={styles.selectedStartDate.color}
             // selectedDayTextStyle={{ color: styles.selectedStartDate.color }}
             
-            todayBackgroundColor={styles.selectedStartDate.color}
-            todayTextStyle={{ color: styles.selectedStartDate.backgroundColor }}
+            // todayBackgroundColor={styles.selectedStartDate.color}
+            // todayTextStyle={{ color: styles.selectedStartDate.backgroundColor }}
             
             customDatesStyles={customDatesStyles}/>
     )
@@ -65,16 +87,20 @@ const styles = StyleSheet.create({
         backgroundColor: '#000'
     },
     todayStyle: {
-        // color: 'red',
-        // backgroundColor: '#fff',
+        color: '#000',
+        backgroundColor: '#fff',
         borderBottomWidth: 3, 
         borderColor: '#000',
         borderRadius: 0
     },
-    todayContainerStyle: {
+    selectedStyle: {
+        color: '#fff',
+        backgroundColor: '#000'
+    },
+    selectedContainerStyle: {
 
     }, 
-    todayTextStyle: {
+    selectedTextStyle: {
         
     }
 })
