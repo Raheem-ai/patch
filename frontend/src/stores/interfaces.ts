59560dadbcc4f21c8beac2eafc332be2ff82e362
Tev'n Powers <tevnpowers@gmail.com>
2022-04-08 16:18:52 -0400

    hook up APIs for creating, editing, and deleting attribute and tag data in frontend

--- frontend/src/stores/interfaces.ts
+++ frontend/src/stores/interfaces.ts
@@ -2,7 +2,7 @@ import { Notification, NotificationResponse } from 'expo-notifications';
 import React from 'react';
 import { Animated } from 'react-native';
 import { ClientSideFormat } from '../../../common/api';
-import { Location, NotificationPayload, NotificationType, Me, HelpRequest, ProtectedUser, RequestStatus, ResponderRequestStatuses, HelpRequestFilter, HelpRequestSortBy, AppSecrets, RequestSkill, TeamFilter, TeamSortBy, UserRole, MinUser, User, EditableUser, EditableMe, PendingUser, PatchUIEventPacket, OrganizationMetadata, Role, PatchPermissions } from '../../../common/models'
+import { Location, NotificationPayload, NotificationType, Me, HelpRequest, ProtectedUser, RequestStatus, ResponderRequestStatuses, HelpRequestFilter, HelpRequestSortBy, AppSecrets, RequestSkill, TeamFilter, TeamSortBy, UserRole, MinUser, User, EditableUser, EditableMe, PendingUser, PatchUIEventPacket, OrganizationMetadata, Role, PatchPermissions, AttributeCategory, Attribute, TagCategory, Tag } from '../../../common/models'
 import { RootStackParamList } from '../types';
 import { getStore } from './meta';
 
@@ -164,7 +164,7 @@ export interface IRequestStore extends IBaseStore {
 }
 
 // TO DO: Add 'tags' and 'attributes'
-export type EditOrganizationData = Pick<OrganizationMetadata, 'roleDefinitions' | 'name'>
+export type EditOrganizationData = Pick<OrganizationMetadata, 'name' | 'roleDefinitions' | 'attributeCategories' | 'tagCategories'>
 
 export interface ITempOrganizationStore extends EditOrganizationData {
     clear(prop?: keyof EditOrganizationData): void
@@ -175,13 +175,43 @@ export namespace IEditOrganizationStore {
 }
 
 export interface IEditOrganizationStore extends ITempOrganizationStore {
+    // Edit Role
     currentRoleName: string
     currentRolePermissions: PatchPermissions[]
 
+    // Edit Attribute Category
+    currentAttributeCategoryName: string
+
+    // Edit Attribute
+    currentAttributeName: string
+
+    // Edit Tag Category
+    currentTagCategoryName: string
+
+    // Edit Tag
+    currentTagName: string
+
     editOrganization(orgId: string): Promise<OrganizationMetadata>
+
     createNewRole(): Promise<Role>
     editRole(roleId: string): Promise<Role>
     deleteRoles(roleIds: string[]): Promise<OrganizationMetadata>
+
+    createNewAttributeCategory(): Promise<AttributeCategory>
+    editAttributeCategory(categoryId: string): Promise<AttributeCategory>
+    deleteAttributeCategory(categoryId: string): Promise<OrganizationMetadata>
+
+    createNewAttribute(categoryId: string): Promise<Attribute>
+    editAttribute(categoryId: string, attributeId: string): Promise<Attribute>
+    deleteAttribute(categoryId: string, attributeId: string): Promise<OrganizationMetadata>
+
+    createNewTagCategory(): Promise<TagCategory>
+    editTagCategory(categoryId: string): Promise<TagCategory>
+    deleteTagCategory(categoryId: string): Promise<OrganizationMetadata>
+
+    createNewTag(categoryId: string): Promise<Tag>
+    editTag(categoryId: string, tagId: string): Promise<Tag>
+    deleteTag(categoryId: string, tagId: string): Promise<OrganizationMetadata>
 }
 
 export namespace IOrganizationStore {
@@ -194,6 +224,10 @@ export interface IOrganizationStore extends IBaseStore {
     getOrgData(): Promise<void>;
     updateOrgData(updatedOrg: OrganizationMetadata): void
     updateOrAddRole(updatedRole: Role): void
+    updateOrAddAttributeCategory(updatedCategory: AttributeCategory): void
+    updateOrAddAttribute(categoryId: string, updatedAttribute: Attribute): void
+    updateOrAddTagCategory(updatedCategory: TagCategory): void
+    updateOrAddTag(categoryId: string, updatedTag: Tag): void
 }
 
 export namespace ITeamStore {
