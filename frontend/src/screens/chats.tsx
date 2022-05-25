144805dea1902a5889a0c34358fbea04f5af2806
Tev'n Powers <tevnpowers@gmail.com>
2022-05-25 19:43:58 -0400

    add permissions check to determine which chats a user should have access to see

--- frontend/src/screens/chats.tsx
+++ frontend/src/screens/chats.tsx
@@ -1,28 +1,37 @@
 import { observer } from "mobx-react";
 import React, { useState } from "react";
-import { Pressable, StyleSheet, View } from "react-native";
+import {StyleSheet, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { Button, IconButton, Text } from "react-native-paper";
-import { RequestSkillToLabelMap, UserRoleToLabelMap } from "../../../common/models";
+import { PatchPermissions } from "../../../common/models";
 import HelpRequestChatPreview from "../components/chats/helpRequestChatPreview";
-import Tags from "../components/tags";
-import { visualDelim } from "../constants";
-import { navigationRef } from "../navigation";
-import { linkingStore, requestStore, userStore } from "../stores/interfaces";
-import { Colors, ScreenProps } from "../types";
+import { requestStore } from "../stores/interfaces";
+import { ScreenProps } from "../types";
+import { iHaveAnyPermissions } from "../utils";
 
 type Props = ScreenProps<'Chats'>;
 
 const Chats = observer(({ navigation, route }: Props) => {
     const [ loading, setLoading ] = useState(false)
-    const requests = () => {
-        if (!requestStore().activeRequests.length) {
+
+    const seeAllRequestChats =  iHaveAnyPermissions([PatchPermissions.SeeAllChats, PatchPermissions.RequestAdmin]);
+
+    const getUserRequests = () => {
+        if (seeAllRequestChats) {
+            // TODO: more to do? set filter?
+            return requestStore().filteredSortedRequests;
+        } else {
+            return requestStore().activeRequests;
+        }
+    }
+
+    const chatFeed = () => {
+        if (!getUserRequests().length) {
             return null
-        } 
+        }
 
         return <View style={styles.requestSection}>
             {
-                requestStore().activeRequests.map((r, idx) => {
+                getUserRequests().map((r, idx) => {
                     const style = !idx
                                   ? { marginTop: 20 }
                                   : null
@@ -40,7 +49,7 @@ const Chats = observer(({ navigation, route }: Props) => {
     }
 
     return <ScrollView showsVerticalScrollIndicator={false}>
-        { requests() }
+        { chatFeed() }
     </ScrollView>
 })
 
