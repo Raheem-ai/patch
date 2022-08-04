842e571cdcf2deacbeab27ade255581457f88cad
Nadav Savio <nadav@giantant.com>
2022-08-04 10:15:18 -0700

    Syncing with staging

--- frontend/src/components/patchButton.tsx
+++ frontend/src/components/patchButton.tsx
@@ -1,46 +1,55 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { Button as RNButton } from "react-native-paper";
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
 import { Colors } from "../types";
-import STRINGS from "../../../common/strings"
 
-type Props = React.ComponentProps<typeof RNButton> & {
-    children: React.ReactNode,
-}
+export type PatchButtonProps = {
+    label: string,
+    width?: number | string,
+    small?: boolean,
+  } & Pick <React.ComponentProps<typeof Button>,
+    'mode'
+    | 'dark'
+    | 'compact'
+    | 'disabled'
+    | 'uppercase'
+    | 'accessibilityLabel'
+    | 'contentStyle'
+    | 'style'
+    | 'labelStyle'
+    | 'onPress'
+  >;
 
-const PatchButton = ({ 
-    mode,
-    onPress,
-    children
-}: Props) => {
+const PatchButton = ( props:PatchButtonProps) => {
+    const {
+        label,
+        width,
+        small,
+        ...rest
+    } = props;
 
-    const uppercase = mode && mode === 'text'
-        ? true
-        : false;
+    const buttonStyle = rest.mode 
+        ? rest.mode == 'outlined'
+            ? styles.outlinedButton
+            : rest.mode == 'contained'
+                ? styles.containedButton
+                : styles.textButton
+        : styles.outlinedButton;
 
-    return (
-        <RNButton 
-            uppercase={uppercase}
-            mode={mode}
-            style={[styles.button, 
-                (mode === 'outlined' 
-                    ? styles.outlineButton
-                    : (mode === 'text')
-                        ? styles.textButton
-                        : null
-            )]}
-            labelStyle={[styles.buttonLabel, 
-                (mode === 'outlined' 
-                    ? styles.outlineLabel
-                    : (mode === 'text')
-                        ? styles.textLabel
-                        : null
-            )]}
-            color={Colors.primary.alpha}
-            onPress={onPress}
-            >
-            {children}
-        </RNButton>
+    const buttonLabelStyle = rest.mode 
+        ? rest.mode == 'outlined'
+            ? styles.outlinedButtonLabel
+            : rest.mode == 'contained'
+                ? styles.containedButtonLabel
+                : styles.textButtonLabel
+        : styles.outlinedButton;
+
+    return(
+        <Button {...rest}
+            style={[ styles.button, buttonStyle, (width ? {width: width} : (buttonStyle == styles.textButton || small ? null : {width: '100%'})), (small && styles.smallButton) ]}
+            labelStyle={[ styles.buttonLabel, buttonLabelStyle, (small && styles.buttonLabelSmall) ]}
+            accessibilityLabel={label}>{label}
+        </Button>
     )
 }
 
@@ -49,28 +58,65 @@ export default PatchButton;
 
 const styles = StyleSheet.create({
     button: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
         borderRadius: 32,
-        color: Colors.text.defaultReversed
+        height: 48,
     },
-    buttonLabel: {
-
+    smallButton: {
+        height: 36,
+        width: null,
     },
-    outlineButton: {
+    containedButton: {
+        backgroundColor: Colors.primary.alpha,
+        color: Colors.text.defaultReversed,
+    },
+    outlinedButton: {
         borderWidth: 1,
+        borderStyle: 'solid',
+        backgroundColor: Colors.backgrounds.standard,
         borderColor: Colors.primary.alpha,
     },
-    outlineLabel: {
-
-    },
     textButton: {
-        borderWidth: 0,
+        backgroundColor: Colors.backgrounds.standard,
+        borderColor: Colors.nocolor,
     },
-    textLabel: {
+    buttonLabel: {
+        letterSpacing: 0.2,
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    buttonLabelSmall: {
+        fontSize: 14,
+    },
+    containedButtonLabel: {
+        color: Colors.text.defaultReversed,
+    },
+    outlinedButtonLabel: {
         color: Colors.primary.alpha,
-        fontWeight: '700',
     },
-    disabled: {
-        // opacity: .8,
-        color: Colors.text.disabled,
-    }
-}) 
+    textButtonLabel: {
+        color: Colors.primary.alpha,
+        textTransform: 'uppercase',
+    },
+})
+
+/*
+Standard Button props:
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+
+*/
