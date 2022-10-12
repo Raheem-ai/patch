2bb08938d0064869bf28069a1a9e2f4378aa112e
Charlie Lipford <charlie.lipford@gmail.com>
2022-10-12 13:11:07 -0400

    fixing working dir for migration scripts

--- ci/backend/revert_migrations.sh
+++ ci/backend/revert_migrations.sh
@@ -3,6 +3,7 @@ if test -f "deployment_failed.txt"; then
     # NOTE: this means each migration should do a deployment so if it fails it is automatically reverted
     # if we have more than one data transformation they should all go into the same migration if they are for the same
     # feature
+    cd /app/backend
     yarn run migration:down --last 
     exit 1
 fi  
