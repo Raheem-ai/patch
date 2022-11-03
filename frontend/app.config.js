01e18e2a0eec2b121842b6507ca73fc8ef5a8b42
Charlie Lipford <charlie.lipford@gmail.com>
2022-11-03 12:55:55 -0400

    version bump

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 // NOTE: put your ngrok url here for development
 let apiHost = ''
 // increment every time you do a build you're going to submit a new release 
-const RELEASE_NUMBER = '0.0.8'
+const RELEASE_NUMBER = '0.0.9'
 // increment this any time you want to submit a new release to the play store
 const BUILD_COUNT = 3
 
