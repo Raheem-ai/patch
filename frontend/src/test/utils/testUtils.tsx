c6f84650f72e89007696d79d54aa7a0ce1719bef
Tev'n Powers <tevnpowers@gmail.com>
2023-03-09 13:35:55 -0500

    add very much needed timeout

--- frontend/src/test/utils/testUtils.tsx
+++ frontend/src/test/utils/testUtils.tsx
@@ -876,6 +876,9 @@ export async function editRequestLocation(getByTestId: GetByQuery<TextMatch, Com
     await act(async () => fireEvent(mapSearchInput, 'focus'));
     await act(async() => fireEvent.changeText(mapSearchInput, searchTerm));
 
+    // Sleep for a second for autocomplete suggestions to be returned
+    await new Promise(r => setTimeout(r, 1000));
+
     // Select the autocomplete suggestion
     const autocompleteSuggestion = await waitFor(() => getByTestId(TestIds.inputs.mapInput.suggestionN(wrappedTestID, 0)));
     await act(async() => fireEvent(autocompleteSuggestion, 'press'));
