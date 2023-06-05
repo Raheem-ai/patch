c7939d80f407247e0490b298d0993acb2c087f41
Tev'n Powers <tevnpowers@gmail.com>
2023-06-05 17:07:25 -0400

    add type and function for generating exhaustive list of props from type

--- common/index.ts
+++ common/index.ts
@@ -1,1 +1,12 @@
 export type AtLeast<T, K extends keyof T> = Partial<T> & Required<Pick<T, K>>;
+
+type AtLeastOne<T> = [T, ...T[]];
+
+export const exhaustiveStringTuple = <T extends string>() =>
+    <L extends AtLeastOne<T>>(
+        ...x: L extends any ? (
+            Exclude<T, L[number]> extends never ? 
+            L : 
+            Exclude<T, L[number]>[]
+    ) : never
+) => x;
