38280ad6b1aa52d3f2d40164721924ad30eb5cf7
Charlie Lipford <charlie.lipford@gmail.com>
2023-08-29 13:29:29 -0400

    [Release] - 99a2129

--- ci/backend/generate_release_config.sh
+++ ci/backend/generate_release_config.sh
@@ -3,3 +3,5 @@
 /app/backend/infra/bin/run config:generate -e $_ENVIRONMENT --toFile secretConfig.txt --secretVersionList --noComments
 # generate config in a format gcloud can comsume them
 /app/backend/infra/bin/run config:generate -e $_ENVIRONMENT --toFile config.txt "--delim=##" --configList --noComments
+# generate just config in a location that build scripts can consume them 
+/app/backend/infra/bin/run config:generate -e $_ENVIRONMENT --toFile /workspace/build_config.env --noComments --config
--- ci/frontend/update_dynamic_config_preprod.sh
+++ ci/frontend/update_dynamic_config_preprod.sh
@@ -2,4 +2,6 @@ if test -f "/workspace/has_native_changes";
 then
     echo "# Add new dynamic AppVersion config entry with 'testing' flag"
     /app/backend/bin dynamicConfig:update --newNativeVersion
+else
+    echo "# No native changes. Skipping dynamic config update"
 fi
