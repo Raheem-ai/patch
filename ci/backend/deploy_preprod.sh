64d3800af57765189402b036b873ff53d75bd351
Charlie Lipford <charlie.lipford@gmail.com>
2023-08-29 14:08:29 -0400

    [Release] - 99a2129

--- ci/backend/deploy_preprod.sh
+++ ci/backend/deploy_preprod.sh
@@ -1,5 +1,7 @@
 echo "# deploying new service revision to preprod"
 
+echo "# gcloud beta run deploy $_SERVICE --image=gcr.io/$PROJECT_ID/patch-rel-pre:$SHORT_SHA --tag=preprod --region=us-central1 --set-secrets=$(cat secretConfig.txt) \"--set-env-vars=^##^$(cat config.txt)\""
+
 if gcloud beta run deploy $_SERVICE --image=gcr.io/$PROJECT_ID/patch-rel-pre:$SHORT_SHA --tag=preprod --region=us-central1 --set-secrets=$(cat secretConfig.txt) "--set-env-vars=^##^$(cat config.txt)";
 then
     echo "Deployed to preprod"
