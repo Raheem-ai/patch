e1acba73a2065abd6e388a344cc41627ec2e0db6
Charlie Lipford <charlie.lipford@gmail.com>
2023-08-31 14:15:26 -0400

    [Release] - 99a2129

--- ci/frontend/update_dynamic_config_preprod.sh
+++ ci/frontend/update_dynamic_config_preprod.sh
@@ -1,7 +1,7 @@
 if test -f "/workspace/has_native_changes";
 then
     echo "# Add new dynamic AppVersion config entry with 'testing' flag"
-    /app/backend/bin dynamicConfig:update --newNativeVersion
+    /app/backend/bin/run dynamicConfig:update --newNativeVersion
 else
     echo "# No native changes. Skipping dynamic config update"
 fi
